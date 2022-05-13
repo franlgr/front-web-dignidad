eedd210078ae80580a100239162e6c8f9b5aa520
surimanw <surimanw@gmail.com>
2022-05-13 20:23:32 -0300

    rarooo

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
     // Global page headers: https://go.nuxtjs.dev/config-head
     head: {
-        title: 'my-nuxt',
+        title: 'mp-rural-dignidad',
         htmlAttrs: {
             lang: 'en',
         },
