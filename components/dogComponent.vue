f609317145290b2e1c8d76e001fe69e6815bccbc
surimanw <surimanw@gmail.com>
2022-05-23 14:44:42 -0300

    adsensce

--- components/dogComponent.vue
+++ components/dogComponent.vue
@@ -13,17 +13,11 @@
     
 </template>
 
-
-
-
 <script>
-
 // import axios from 'axios'
-
      export default {
         data(){
            return{
-
             chat_id: "2047819373",
             file_id: 'AgACAgEAAxkBAAPAYndohEskxAEqz4lZJG5pczAOnuAAApipMRuXncBHyQ0G7oTOdhkBAAMCAANzAAMkBA',
             file:[],
@@ -37,21 +31,16 @@
             }
         },
             methods: {
-
                      sendMsg(){
-
-                        this.$axios.post('https://api.telegram.org/bot5345020457:AAF5K4979ne8sKV3B4SWuUf-0HM4SrwBm_Y/sendMessage', {
-    
+                        this.$axios.post('https://api.telegram.org/bot5345020457:AAF5K4979ne8sKV3B4SWuUf-0HM4SrwBm_Y/getUpdates', {
                           chat_id: this.chat_id,
                           // file_id: this.file_id,
                         //   ctx: this.contenido.text,
                           files: `Imagen: ${this.file}`,
                         //   text: `Nombres: ${this.contenido.names} Mail: ${this.contenido.email} Mensaje: ${this.contenido.text}`,
                         text: this.contenido.text,
-
                           }).then((response) =>{
                               console.log("nuevo: ", response);
-
                           }).catch(function (error) {
                               console.log(error);
                        });
